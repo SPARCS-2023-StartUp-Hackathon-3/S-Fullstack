32817f042c8216f2ed1c188aba3c1042176cc05f
Me1e <mele0404@naver.com>
2023-01-28 01:26:18 +0900

    fix: font 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import localFont from '@next/font/local';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={pretendard.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
