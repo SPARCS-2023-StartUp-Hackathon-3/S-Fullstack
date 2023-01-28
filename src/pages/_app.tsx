8a856d30df41843f9a1ce171458d5852fb337c5a
Me1e <mele0404@naver.com>
2023-01-28 10:06:24 +0900

    feat: react-query 준비

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import localFont from '@next/font/local';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
 });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={pretendard.className}>
-      <Component {...pageProps} />;
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className={pretendard.className}>
+        <Component {...pageProps} />
+      </main>
+    </QueryClientProvider>
   );
 }
