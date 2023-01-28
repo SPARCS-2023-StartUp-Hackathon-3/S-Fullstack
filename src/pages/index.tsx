83f402c9d5aaeea8f390780f7c642385394714c3
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-01-28 18:39:24 +0900

    feat(Index): replace to explore if signed in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
-import { BottomTabsNavigator } from '@/components/BottomTabsNavigator';
 import SignIn from '@/components/SignIn';
+import { useUserInfoStore } from '@/util/store';
+import { useRouter } from 'next/router';
 
 export default function Page() {
+  const { username } = useUserInfoStore();
+  const router = useRouter();
+
+  if (username !== '') router.replace('/explore');
+
   return <SignIn />;
 }
