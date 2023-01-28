2cfa99381ca14bbeb99e4959ffd30687c51bc833
Me1e <mele0404@naver.com>
2023-01-28 10:06:07 +0900

    feat(store): generate 로직 store

--- src/util/store.ts
+++ src/util/store.ts
@@ -1,17 +1,49 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const useStore = create<any>(
-  persist(
+interface IGenerateStore {
+  title: string;
+  color: string;
+  desc: string;
+  imageUrl: string;
+  isNew: boolean;
+  setTitle: (data: string) => void;
+  setColor: (data: string) => void;
+  setDesc: (data: string) => void;
+  setImg: (data: string) => void;
+  setIsNew: (data: boolean) => void;
+  resetAll: () => void;
+}
+
+export const useGenerateStore = create(
+  persist<IGenerateStore>(
     (set) => ({
-      testData: [],
-      setTestData: (select: any) => {
-        set((state: any) => ({ ...state, testData: select }));
+      title: '',
+      color: '',
+      desc: '',
+      imageUrl: '',
+      isNew: true,
+      setTitle: (data) => {
+        set((state) => ({ ...state, title: data }));
       },
-      resetData: () => {
-        set((state: any) => ({ ...state, testData: [] }));
+      setColor: (data) => {
+        set((state) => ({ ...state, color: data }));
+      },
+      setDesc: (data) => {
+        set((state) => ({ ...state, desc: data }));
+      },
+      setImg: (data) => {
+        set((state) => ({ ...state, imageUrl: data }));
+      },
+      setIsNew: (data) => {
+        set((state) => ({ ...state, isNew: data }));
+      },
+      resetAll: () => {
+        set({ title: '', color: '', desc: '', imageUrl: '', isNew: true });
       },
     }),
-    { name: 'user-StoreName' }
+    {
+      name: 'generate-store',
+    }
   )
 );
